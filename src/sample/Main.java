a627f413440f59aedbc31c64d867a493bcd61ce5
fazel <amirfazel45@gmail.com>
2021-07-20 10:25:51 +0430

    linking the dummyGame fxml and stuff to main Application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,11 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new  FXMLLoader(getClass().getResource("View/dummyGame.fxml"));
+        FXMLLoader loader = new  FXMLLoader(getClass().getResource("View/Entrance.fxml"));
         Parent root =loader.load();
         primaryStage.setTitle("JavaFX is my nigga");
         Scene scene = new Scene(root);
-        String css = this.getClass().getResource("View/StyleSheets/DummyGame.css").toExternalForm();
+        String css = this.getClass().getResource("View/StyleSheets/Phase1.css").toExternalForm();
         scene.getStylesheets().add(css);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
